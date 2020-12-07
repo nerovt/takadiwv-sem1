b4d5b0126ea1ccb5dc82a813159cc9c93fde14b8
Victor Takadiwa <takadiwv@coventry.ac.uk>
2020-12-07 18:07:44 +0000

    Wrote unit tests for 'items.js'

---
+++ unitTests/test-items.js
@@ -0,0 +1,68 @@
+
+import test from 'ava'
+import Items from '../modules/items.js'
+
+test('REGISTER : error if blank item', async test => {
+	test.plan(1)
+	const items = await new Items()
+	try {
+		await items.register('', './images/fridge.png', 'sold', 'victor', '1234543')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		items.close()
+	}
+})
+
+test('REGISTER : error if blank image', async test => {
+	test.plan(1)
+	const items = await new Items()
+	try {
+		await items.register('fridge', '', 'sold', 'victor', '1234543')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		items.close()
+	}
+})
+
+test('REGISTER : error if blank status', async test => {
+	test.plan(1)
+	const items = await new Items()
+	try {
+		await items.register('fridge', './images/fridge.png', '', 'victor', '1234543')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		items.close()
+	}
+})
+
+test('REGISTER : error if blank seller', async test => {
+	test.plan(1)
+	const items = await new Items()
+	try {
+		await items.register('fridge', './images/fridge.png', 'sold', '', '1234543')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		items.close()
+	}
+})
+
+test('REGISTER : error if blank phoneNumber', async test => {
+	test.plan(1)
+	const items = await new Items()
+	try {
+		await items.register('fridge', './images/fridge.png', 'sold', 'victor', '')
+		test.fail('error not thrown')
+	} catch(err) {
+		test.is(err.message, 'missing field', 'incorrect error message')
+	} finally {
+		items.close()
+	}
+})
