4d742ec8d3564d974f7dff7bd386993a7c454557
Victor Takadiwa <takadiwv@coventry.ac.uk>
2020-12-09 22:27:29 +0000

    GET request & display

--- routes/auc.js
+++ routes/auc.js
@@ -6,18 +6,47 @@ const router = new Router({ prefix: '/auc' })
 async function checkAuth(ctx, next) {
 	console.log('secure router middleware')
 	console.log(ctx.hbs)
-	if(ctx.hbs.authorised !== true) return ctx.redirect('/login?msg=you need to log in&referrer=/secure')
+	if(ctx.hbs.authorised !== true) return ctx.redirect('/login?msg=you need to log in&referrer=/auc')
 	await next()
 }
 
 router.use(checkAuth)
+import Accounts from '../modules/accounts.js'
+import Items from '../modules/items.js'
+const dbName = 'website.db'
 
+function modifyResults(results, ctx) {
+	const items = []
+	for(const i of results) {
+		if(String(i.seller) === String(ctx.session.username)) {
+			items.push(i)
+		}
+	}
+	for(const i of items) {
+		i = {id: i.id, item: i.item, image: i.image, status: i.status}
+	}
+	return items
+}
+
+/**
+ * The secure page.
+ *
+ * @name Auction Page
+ * @route {GET} /
+ */
 router.get('/', async ctx => {
+	const account = await new Accounts(dbName)
+	const items = await new Items(dbName)
 	try {
+		let results = await items.getDetails()
+		results = modifyResults(results, ctx) //call function to modify
+		ctx.hbs.record = [] ; ctx.hbs.record = results
 		await ctx.render('auc', ctx.hbs)
 	} catch(err) {
-		ctx.hbs.error = err.message
+		console.log(err.message)
 		await ctx.render('error', ctx.hbs)
+	} finally {
+		account.close() ; items.close()
 	}
 })
 
